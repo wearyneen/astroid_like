9d060e5a68d4da40c937eaa40bd64e8acddb8f88
wearyneen <randomstuff666@gmail.com>
2024-12-30 08:42:52 +0000

    create gameloop

--- main.py
+++ main.py
@@ -4,12 +4,21 @@
 import pygame
 from constants import *
 
+
 #main function
 def main():
+    #initalise pygame
+    pygame.init()
     print("Starting asteroids!")
     #Print out the SCREEN_WIDTH and SCREEN_HEIGHT values when main.py is run. Use the following format:
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    #create gui window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #initiate game loop
+    while True:
+        screen.fill('black')
+        pygame.display.flip()
     
 
 #ensure only runs if file is run
