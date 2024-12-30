7368f11b5bcf01837157cef13b1aea89b13c0dad
wearyneen <randomstuff666@gmail.com>
2024-12-30 08:45:43 +0000

    enable exit button

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #initiate game loop
     while True:
+        #enable X button functionality
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill('black')
         pygame.display.flip()
     
