f9dd4c11b9f5bda5de93aef06de24d77fba1236a
wearyneen <randomstuff666@gmail.com>
2024-12-30 08:09:48 +0000

    create main and enable virtual enviroment

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+#main function
+def main():
+    print("Starting asteroids!")
+
+
+#ensure only runs if file is run
+if __name__ == "__main__":
+    main()
