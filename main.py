18f44cd8cfdbb6053e6cb8eb7cedf71140110f9a
wearyneen <randomstuff666@gmail.com>
2024-12-30 10:22:13 +0000

    added player movement left/right

--- main.py
+++ main.py
@@ -1,43 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from player import Player
 from constants import *
+from player import Player
 
 
-#main function
 def main():
-    #initalise pygame
     pygame.init()
-    #create gui window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #restrict fps
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    #initiate player
-    player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT/2))
-    
-    #initiate game loop
+
     while True:
-        #enable X button functionality
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill('black')
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-        
 
-        #fps restriction
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
-    
-
-#ensure only runs if file is run
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -22,3 +22,15 @@ class Player(CircleShape):
     def draw(self, screen):
         player_icon = pygame.draw.polygon(screen, 'white', self.triangle(), width=2)
         return player_icon
+    
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
