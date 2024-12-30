0547ebf12888ccb22031bc2a16e68a63d99695e4
wearyneen <randomstuff666@gmail.com>
2024-12-30 08:20:27 +0000

    import constants into main

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 #main function
 def main():
     print("Starting asteroids!")
-
+    #Print out the SCREEN_WIDTH and SCREEN_HEIGHT values when main.py is run. Use the following format:
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 #ensure only runs if file is run
 if __name__ == "__main__":
