251d997f6462dc8217989ba6eaa26d478758039b
wearyneen <randomstuff666@gmail.com>
2024-12-30 15:53:37 +0000

    create collision behaviour

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, circle_shape):
+
+        return self.position.distance_to(circle_shape.position) <= self.radius + circle_shape.radius
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -28,6 +29,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
